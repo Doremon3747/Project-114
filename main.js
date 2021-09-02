806263e0fa5729029f3f1d954c38ad95eb068f5e
Doremon3747 <84011096+Doremon3747@users.noreply.github.com>
2021-09-02 17:58:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(230,230);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save("filter face  Image.png");
+}
